0af4f86a1fb8266eb8aad7b61290038d7a241e9c
ferreiravih <vitoria2307@gmail.com>
2025-08-21 18:03:45 -0300

    Componentes reutilizáveis e login

---
+++ componentes/footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="../global/global.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+
+<body>
+    <footer class="footerb">
+        <div class="footercontainer">
+            <div class="foot">
+                <h3> suporte </h3>
+                <p> De segunda a sexta, das 09h ás 17h</p>
+                <address>
+                    <p><a><i class="fa-solid fa-envelope" style="color: #ff934d;"></i>  suporteskatelab@gmail.com </a></p>
+                    <p><a><i class="fa-solid fa-phone" style="color: #ff934d;"></i>  (11) 9xxxx-xxxx </a></p>
+                </address>
+            </div>
+            <div class="foot">
+                <h3>Explore</h3>
+                <ul class="foot">
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Customizar</a></li>
+                    <li><a href="#">Downloads</a></li>
+                </ul>
+            </div>
+            <div class="foot">
+                <h3>Mais</h3>
+                <ul class="foot">
+                    <li><a>ações Rápidas</a></li>
+                    <li><a>poítica de privicidade</a></li>
+                    <li><a>Políticas de troca, devolução e conserto</a></li>
+                </ul>
+            </div>
+            <div class="foot">
+                <h3> Redes Sociais</h3>
+                <ul class="foots">
+                    <li><a><i class="fa-brands fa-instagram fa-2xl" style="color: #ff914d;"></i></a></li>
+                    <li><a><i class="fa-brands fa-youtube fa-2xl" style="color: #ff914d;"></i></a></li>
+                    <li><a><i class="fa-brands fa-facebook fa-2xl" style="color: #ff914d;"></i></a></li>
+                    <li><a><i class="fa-brands fa-tiktok fa-2xl" style="color: #ff914d;"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <button><i class="fa-solid fa-headphones" style="color: #ff914d;"></i>fale conosco </button>
+        <hr>
+    </footer>
+</body>
+
+</html>
