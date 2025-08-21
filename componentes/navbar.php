6f0c08701b51955be728f864a20390d9868b5e42
ferreiravih <vitoria2307@gmail.com>
2025-08-21 18:03:45 -0300

    Componentes reutilizáveis e login

---
+++ componentes/navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>skatelab</title>
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="../global/global.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nosifer&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+</head>
+
+<body>
+    <header class="headerb">
+        <h1 class="logo">SKATELAB</h1>
+        <ul class="lista">
+            <li><a href="../skateshop/index.php">skateshop</a></li>
+            <li><a href="destino">customizar</a></li>
+            <li><a href=""><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></a></li>
+            <li><a href=""><i class="fa-solid fa-headset" style="color: #ffffffff;"></i></a></li>
+            <li><a href=""><i class="fa-solid fa-truck" style="color: #ffffff;"></i></a></li>
+            <li><a href=""><i class="fa-solid fa-circle-user" style="color: #ffffff;"></i> </a></li>
+        </ul>
+    </header>
+</body>
+</html>
